28975c7909b297139c6d621bb5181adea513a335
AngelineTung <angelinetung@hotmail.com>
2025-10-22 11:02:45 +0800

    13th commit

--- .github/workflows/terraform/backend.tf
+++ .github/workflows/terraform/backend.tf
@@ -1,11 +1,9 @@
 terraform {
-  required_version = ">= 1.5.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
+  backend "s3" {
+    bucket         = "my-tfstate-bucket-prod"
+    key            = "terraform/terraform.tfstate"
+    region         = "ap-southeast-1"
+    dynamodb_table = "my-tf-locks-prod"
+    encrypt        = true
   }
-
-  backend "s3" {}  # values passed via -backend-config in your workflow
 }
